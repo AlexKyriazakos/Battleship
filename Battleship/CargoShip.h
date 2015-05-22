beef0df97d0cb3cbe2a37c5b25b7f2ed77c031fd
Alex <alexkyr@gmail.com>
2015-05-22 14:44:44 +0300

    stuck errors

--- Battleship/CargoShip.h
+++ Battleship/CargoShip.h
@@ -11,7 +11,19 @@ public:
 	//Why virtual again????
 	virtual void action()
 	{
-		//do nothing
+		for (int i = location.row - 1; i <= location.row + 1; ++i)
+		{
+			for (int j = location.col - 1; j <= location.col + 1; ++j)
+			{
+				if (i >= 0 && i < map->grid.size() && j >= 0 && j < map->grid.size())
+				{
+					if (map->grid[i][j].hasPort())
+						treasure++;
+				}
+			}
+		}
+			
+
 	}
 
 	virtual std::string getName(bool extended) const
