b7e0ce56e6b1d77364162e508353052a0d2c1bd4
Alex <alexkyr@gmail.com>
2015-05-23 15:45:32 +0300

    stuck errors v3

--- Battleship/CargoShip.h
+++ Battleship/CargoShip.h
@@ -11,17 +11,7 @@ public:
 	//Why virtual again????
 	virtual void action()
 	{
-		std::vector<Cell*> NeighbourCells;
-		for (int i = location.row - 1; i <= location.row + 1; ++i)
-		{
-			for (int j = location.col - 1; j <= location.col + 1; ++j)
-			{
-				if (i >= 0 && i < 12 && j >= 0 && j < 12)
-				{
-					NeighbourCells.push_back(map->getCell(i, j));
-				}
-			}
-		}
+		std::vector<Cell*> neighbourCells = map->findNeighbourCells(*this);
 			
 
 	}
