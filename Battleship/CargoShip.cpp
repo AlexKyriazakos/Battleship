9c17a93e52298fc77600d0580d385e5180cc6250
Alex <alexkyr@gmail.com>
2015-05-23 20:26:28 +0300

    gko spri

--- Battleship/CargoShip.cpp
+++ Battleship/CargoShip.cpp
@@ -1,9 +1,14 @@
 #include "CargoShip.h"
 #include "Map.h"
 
-class Cell;
+
 
 void CargoShip::action()
 {
 	std::vector<Cell*> neighbourCells = map->findNeighbourCells(*this);
+	for (int i=0; i != neighbourCells.size(); i++)
+	{
+		if (neighbourCells[i]->hasPort())
+			treasure++;
+	}
 }
