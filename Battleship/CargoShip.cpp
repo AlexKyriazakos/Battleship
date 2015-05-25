e85ea5ddafe5632053f93f5c78fad6d32b6cb58b
Victor Kyriazakos <victor@kyriazakos.eu>
2015-05-25 03:56:51 +0300

    gamimena merge

--- Battleship/CargoShip.cpp
+++ Battleship/CargoShip.cpp
@@ -1,11 +1,6 @@
 #include "CargoShip.h"
 #include "Map.h"
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 31d37860ffd8c1d3fadf1d237dc9e3b4bef94b28
-
 void CargoShip::action()
 {
 	std::vector<Cell*> neighbourCells = map->findNeighbourCells(*this);
